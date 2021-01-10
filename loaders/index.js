a6a3fefc92ed581ba6bd3403ec5ce9d16e59824c
Ryan Durling <ryan.p.durling@gmail.com>
2021-01-10 13:45:08 -0600

    load JS

--- loaders/index.js
+++ loaders/index.js
@@ -1,9 +1,11 @@
 const {configCSS} = require('./configCSS');
 const {loadImages} = require('./loadImages');
-const {loadTS} = require('./loadTS')
+const {loadTS} = require('./loadTS');
+const {loadJS} = require('./loadTS');
 
 module.exports = {
-    configCSS,
-    loadImages,
-    loadTS
+  configCSS,
+  loadImages,
+  loadTS,
+  loadJS
 };
